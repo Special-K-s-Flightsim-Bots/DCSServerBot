a2e0cb90d2024fc52a091fd42743ebc8aa4ddb2a
karel26 <41538366+karel26@users.noreply.github.com>
2021-12-24 15:14:29 +0100

    BUGFIX: - Bot got in bad state on discord reconnects. - callback() didn't work correctly in mission environment - removed warnRestart() from mission environment due to DCS 2.7.9 net changes - changed sanitization to be compatible with DCS 2.7.9 - changed handling of player connects to avoid exceptions on direct disconnects
    Added:
    - AUDIT_CHANNEL and lots of audit messages
    - Improved matching algorithm
    - Automatch on discord join
    - New commands .info and .linkcheck
    - Automatic name changes of the discord status channels to display the number of users [x/y]

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ from os import path
 from psycopg2 import pool
 
 # Set the bot's version (not externally configurable)
-BOT_VERSION = '2.5.1'
+BOT_VERSION = '2.5.2'
 
 LOGLEVEL = {
     'DEBUG': logging.DEBUG,
