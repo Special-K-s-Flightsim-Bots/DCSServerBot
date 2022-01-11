bbaecdc36775bd0acafc98477fd2c27296af1090
karel26 <41538366+karel26@users.noreply.github.com>
2022-01-11 17:50:48 +0100

    Update tables.sql
    BUGFIX: tables.sql missed the last_seen column for players

--- sql/tables.sql
+++ sql/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS version (version TEXT PRIMARY KEY);
 INSERT INTO version (version) VALUES ('v1.3') ON CONFLICT (version) DO NOTHING;
 CREATE TABLE IF NOT EXISTS servers (server_name TEXT PRIMARY KEY, agent_host TEXT NOT NULL, host TEXT NOT NULL DEFAULT '127.0.0.1', port BIGINT NOT NULL, chat_channel BIGINT, status_channel BIGINT, admin_channel BIGINT);
 CREATE TABLE IF NOT EXISTS message_persistence (server_name TEXT NOT NULL, embed_name TEXT NOT NULL, embed BIGINT NOT NULL, PRIMARY KEY (server_name, embed_name));
-CREATE TABLE IF NOT EXISTS players (ucid TEXT PRIMARY KEY, discord_id BIGINT, name TEXT);
+CREATE TABLE IF NOT EXISTS players (ucid TEXT PRIMARY KEY, discord_id BIGINT, name TEXT, last_seen TIMESTAMP);
 CREATE TABLE IF NOT EXISTS bans (ucid TEXT PRIMARY KEY, banned_by TEXT NOT NULL, reason TEXT, banned_at TIMESTAMP NOT NULL DEFAULT NOW());
 CREATE INDEX IF NOT EXISTS idx_players_discord_id ON players(discord_id);
 CREATE TABLE IF NOT EXISTS missions (id SERIAL PRIMARY KEY, server_name TEXT NOT NULL, mission_name TEXT NOT NULL, mission_theatre TEXT NOT NULL, mission_start TIMESTAMP NOT NULL DEFAULT NOW(), mission_end TIMESTAMP);
