c353f5557b132dbb4171069b85586f9c7ca90c70
karel26 <41538366+karel26@users.noreply.github.com>
2022-01-13 17:42:56 +0100

    ENHANCEMENT: - enhanced logging in lua code - dynamic loading of DCSServerBot.lua in missions added - Mission statistics are configurable now

--- plugins/gamemaster/listener.py
+++ plugins/gamemaster/listener.py
@@ -1,23 +1,11 @@
 from core import EventListener
 
 
-class SampleEventListener(EventListener):
-    """
-    A class where your DCS events will be handled.
+class GameMasterEventListener(EventListener):
 
-    Methods
-    -------
-    registerDCSServer(data)
-        Called on registration of any DCS server.
-
-    sample(data)
-        Called whenever ".sample" is called in discord (see commands.py).
-    """
-
-    async def registerDCSServer(self, data):
-        self.log.debug(f"I've received a registration event from server {data['sever_name']}!")
-
-    async def sample(self, data):
-        self.log.debug("I've received the sample event!")
-        # this is a synchronous call, so we just return the data received
-        return data
+    async def onChatMessage(self, data):
+        chat_channel = self.bot.get_bot_channel(data, 'chat_channel')
+        if chat_channel is not None:
+            if 'from_id' in data and data['from_id'] != 1 and len(data['message']) > 0:
+                return await chat_channel.send(data['from_name'] + ': ' + data['message'])
+        return None
