363fc04767fced7aaa0120848b8f81e00edfc42e
karel26 <Karel2133>
2022-02-19 11:00:53 +0100

    ENHANCEMENTS: - Added OPT_PLUGINS section to the configuration.

--- core/bot.py
+++ core/bot.py
@@ -31,7 +31,10 @@ class DCSServerBot(commands.Bot):
         self.pool = kwargs['pool']
         self.log = kwargs['log']
         self.config = kwargs['config']
-        self.plugins = [p.strip() for p in self.config['BOT']['PLUGINS'].split(',')]
+        plugins = self.config['BOT']['PLUGINS']
+        if 'OPT_PLUGINS' in self.config['BOT']:
+            plugins += ', ' + self.config['BOT']['OPT_PLUGINS']
+        self.plugins = [p.strip() for p in plugins.split(',')]
         self.audit_channel = None
         self.player_data = None
         self.executor = ThreadPoolExecutor(max_workers=10)
