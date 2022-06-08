a05c34db300f38042f59de3d6ceb0c02490608ae
karel26 <Karel2133>
2022-06-08 17:33:49 +0200

    BUGFIX: - Issue with campaigns table

--- plugins/gamemaster/db/update_v1.0.sql
+++ plugins/gamemaster/db/update_v1.0.sql
@@ -1,5 +1,6 @@
 UPDATE plugins SET version = 'v1.1' WHERE plugin = 'gamemaster';
 CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE TABLE IF NOT EXISTS campaigns (campaign_id SERIAL PRIMARY KEY, server_name TEXT, mission_name TEXT);
 CREATE TABLE IF NOT EXISTS campaigns2 (id SERIAL PRIMARY KEY, name TEXT NOT NULL, description TEXT, server_name TEXT NOT NULL, start TIMESTAMP NOT NULL DEFAULT NOW(), stop TIMESTAMP);
 CREATE UNIQUE INDEX IF NOT EXISTS idx_campaigns_key on campaigns2 (server_name, name);
 INSERT INTO campaigns2 (name, server_name, start) SELECT 'Initial', server_name, TO_DATE('01.01.1900', 'DD.MM.YYYY') FROM campaigns c WHERE campaign_id = (select max(campaign_id) FROM campaigns WHERE server_name = c.server_name);
