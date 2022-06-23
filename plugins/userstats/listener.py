5ef64ed80c56b6a53defa69bc19fa02305a41ed2
karel26 <41538366+karel26@users.noreply.github.com>
2022-06-23 09:35:28 +0200

    BUGFIX: issue in -linkme

--- plugins/userstats/listener.py
+++ plugins/userstats/listener.py
@@ -413,8 +413,8 @@ class UserStatisticsEventListener(EventListener):
                                     player['name'], player['ucid']))
                         else:
                             discord_id = cursor.fetchone()[0]
-                            cursor.execute('UPDATE players SET discord_id = %s, manual = TRUE WHERE ucid = %s ON '
-                                           'CONFLICT DO NOTHING', (discord_id, player['ucid']))
+                            cursor.execute('UPDATE players SET discord_id = %s, manual = TRUE WHERE ucid = %s',
+                                           (discord_id, player['ucid']))
                             cursor.execute('DELETE FROM players WHERE ucid = %s', (token, ))
                             utils.sendChatMessage(self, data['server_name'], data['from_id'],
                                                   'Your user has been linked! You must reconnect once for the '
