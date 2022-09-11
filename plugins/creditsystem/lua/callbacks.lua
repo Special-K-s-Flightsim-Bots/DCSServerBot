eb33ec58505cc5c2a016b9cf394fc30b8281413e
karel26 <Karel2133>
2022-09-11 20:02:10 +0200

    BUGFIX: - CreditSystem: mission.lua was not loaded

--- plugins/creditsystem/lua/callbacks.lua
+++ plugins/creditsystem/lua/callbacks.lua
@@ -5,3 +5,5 @@ function creditsystem.onMissionLoadEnd()
     net.dostring_in('mission', 'a_do_script("dofile(\\"' .. lfs.writedir():gsub('\\', '/') .. 'Scripts/net/DCSServerBot/DCSServerBot.lua' .. '\\")")')
     net.dostring_in('mission', 'a_do_script("dofile(\\"' .. lfs.writedir():gsub('\\', '/') .. 'Scripts/net/DCSServerBot/creditsystem/mission.lua' .. '\\")")')
 end
+
+DCS.setUserCallbacks(creditsystem)
