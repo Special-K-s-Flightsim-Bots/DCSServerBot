e17af3db5dc2fcfd3fd3be12362a75898384bffd
karel26 <dcs.specialk@gmail.com>
2022-12-12 17:47:20 +0100

    ENHANCEMENTS: - Admin: added .config command to configure several server settings
    CHANGES:
    - Some internal cleanups of utility methods

--- plugins/admin/commands.py
+++ plugins/admin/commands.py
@@ -183,8 +183,8 @@ class Agent(Plugin):
         if not server:
             return
         if server.status in [Status.RUNNING, Status.PAUSED]:
-            if utils.yn_question(ctx, question='Server has to be stopped to change its configuration.\n'
-                                               'Do you want to stop it?'):
+            if await utils.yn_question(ctx, question='Server has to be stopped to change its configuration.\n'
+                                                     'Do you want to stop it?'):
                 await server.stop()
             else:
                 await ctx.send('Aborted.')
@@ -221,7 +221,7 @@ class Agent(Plugin):
 
             @discord.ui.button(label='Cancel', style=discord.ButtonStyle.secondary, custom_id='cfg_cancel', emoji='❌')
             async def on_cancel(self, interaction: Interaction, button: Button):
-                await interaction.response.defer()
+                await interaction.response.send_message('Aborted.')
                 self.stop()
 
             async def interaction_check(self, interaction: Interaction, /) -> bool:
@@ -280,7 +280,7 @@ class Agent(Plugin):
 
             @discord.ui.button(label='Cancel', style=discord.ButtonStyle.secondary, emoji='❌')
             async def cancel(self, interaction: Interaction, button: Button):
-                await interaction.response.defer()
+                await interaction.response.send_message('Aborted.')
                 self.stop()
 
             async def interaction_check(self, interaction: Interaction, /) -> bool:
