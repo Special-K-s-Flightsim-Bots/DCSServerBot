f0977d2122d802d70cb77ca81c4f5a8ab6153b3b
karel26 <dcs.specialk@gmail.com>
2023-01-31 12:47:06 +0100

    CHANGES: - Cloud: optimized ban system

--- plugins/cloud/listener.py
+++ plugins/cloud/listener.py
@@ -11,20 +11,6 @@ class CloudListener(EventListener):
     def __init__(self, plugin):
         super().__init__(plugin)
 
-    async def registerDCSServer(self, data):
-        # if the server is running, the bans will be sent by the plugin
-        if 'sync-' not in data['channel']:
-            server: Server = self.bot.servers[data['server_name']]
-            try:
-                for ban in (await self.plugin.get('bans')):
-                    server.sendtoDCS({
-                        "command": "ban",
-                        "ucid": ban["ucid"],
-                        "reason": ban["reason"]
-                    })
-            except aiohttp.ClientError:
-                self.log.error('- Cloud service not responding.')
-
     async def onPlayerChangeSlot(self, data: dict) -> None:
         if 'side' not in data or data['id'] == 1:
             return
