c11a802951f19a2fccd2d798c580640c9c96ddec
karel26 <41538366+karel26@users.noreply.github.com>
2023-02-08 11:42:58 +0100

    Optimized migration of old data.

--- sql/update_v1.2.sql
+++ sql/update_v1.2.sql
@@ -1,2 +1,3 @@
-ALTER TABLE players ADD COLUMN name TEXT, ADD COLUMN last_seen TIMESTAMP DEFAULT NOW();
+ALTER TABLE players ADD COLUMN name TEXT, ADD COLUMN last_seen TIMESTAMP;
+UPDATE players p SET last_seen = (SELECT MAX(hop_off) FROM statistics WHERE player_ucid = p.ucid);
 UPDATE version SET version='v1.3';
