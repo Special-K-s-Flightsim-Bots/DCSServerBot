50a53d3fdf55ad0cf4a8e50493e856979c1fd41b
karel26 <dcs.specialk@gmail.com>
2023-02-08 11:30:01 +0100

    BUGFIX: - Updates from very old versions clear the PLAYERS table.

--- sql/update_v1.2.sql
+++ sql/update_v1.2.sql
@@ -1,2 +1,2 @@
-ALTER TABLE players ADD COLUMN name TEXT, ADD COLUMN last_seen TIMESTAMP;
+ALTER TABLE players ADD COLUMN name TEXT, ADD COLUMN last_seen TIMESTAMP DEFAULT NOW();
 UPDATE version SET version='v1.3';
