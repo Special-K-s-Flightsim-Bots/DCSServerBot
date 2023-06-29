1107f4a9a3f9872f3aec023f1344bde5dcc74bf1
karel26 <dcs.specialk@gmail.com>
2023-06-29 18:54:32 +0200

    BUGFIX: - OvGM: Applied the bugfix with non-existing dirs to zip files

--- plugins/ovgme/commands.py
+++ plugins/ovgme/commands.py
@@ -163,7 +163,7 @@ class OvGME(Plugin):
                                     log.write(f"x {name}\n")
                                     dest = os.path.join(ovgme_path, name)
                                     os.makedirs(os.path.dirname(dest), exist_ok=True)
-                                    shutil.copy2(orig, os.path.join(ovgme_path, name))
+                                    shutil.copy2(orig, dest)
                                 else:
                                     log.write(f"w {name}\n")
                                 zfile.extract(name, target)
