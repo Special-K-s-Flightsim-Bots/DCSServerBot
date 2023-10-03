db9740bcecc09c0e6dcc539cb817f76b238c19af
karel26 <dcs.specialk@gmail.com>
2023-10-03 10:25:42 +0200

    BUGFIX: - Wrong message about tacview not being sent

--- extensions/tacview.py
+++ extensions/tacview.py
@@ -201,6 +201,7 @@ class Tacview(Extension):
                                 shutil.copy2(filename, os.path.expandvars(utils.format_string(target, server=self.server)))
                             except Exception as ex:
                                 self.log.warning(f"Can't upload TACVIEW file {filename} to {target}: ", exc_info=ex)
+                            break
                     await asyncio.sleep(1)
                 else:
                     self.log.warning(f"Can't find TACVIEW file {filename} after 1 min of waiting.")
