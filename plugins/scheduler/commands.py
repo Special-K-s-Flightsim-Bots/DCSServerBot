68f3826aeafdf1551370472c734db47251ae92ec
karel26 <dcs.specialk@gmail.com>
2023-10-08 20:04:58 +0200

    BUGFIX: - Name clash of new migrate function

--- plugins/scheduler/commands.py
+++ plugins/scheduler/commands.py
@@ -510,10 +510,10 @@ class Scheduler(Plugin):
         await server.rename(new_name, True)
         await interaction.response.send_message(f"Server {old_name} renamed to {new_name}.", ephemeral=True)
 
-    @group.command(description="Migrate a server from one instance to another")
+    @group.command(name="migrate", description="Migrate a server from one instance to another")
     @app_commands.guild_only()
     @utils.app_has_role('Admin')
-    async def migrate(self, interaction: discord.Interaction,
+    async def _migrate(self, interaction: discord.Interaction,
                       server: app_commands.Transform[Server, utils.ServerTransformer],
                       node: app_commands.Transform[Node, utils.NodeTransformer],
                       instance: app_commands.Transform[Instance, utils.InstanceTransformer]):
