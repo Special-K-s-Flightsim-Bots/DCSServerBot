6c05fde1fef2a741a2f73c1c4a7a33c497109e91
karel26 <dcs.specialk@gmail.com>
2023-11-01 18:39:50 +0100

    CHANGES: - Auto-delete doubled servers in the servers table during the migration

--- sql/update_v1.6.sql
+++ sql/update_v1.6.sql
@@ -1,5 +1,6 @@
 ALTER TABLE servers DROP COLUMN host;
 ALTER TABLE servers RENAME COLUMN agent_host TO node;
+DELETE FROM servers WHERE (node, port, last_seen) NOT IN (SELECT node, port, MAX(last_seen) AS "last_seen" FROM servers GROUP BY node, port);
 CREATE UNIQUE INDEX IF NOT EXISTS idx_servers ON servers (node, port);
 CREATE TABLE IF NOT EXISTS intercom (id SERIAL PRIMARY KEY, node TEXT NOT NULL, time TIMESTAMP NOT NULL DEFAULT NOW(), data JSON);
 CREATE TABLE IF NOT EXISTS nodes (guild_id BIGINT NOT NULL, node TEXT NOT NULL, master BOOLEAN NOT NULL, last_seen TIMESTAMP DEFAULT NOW(), PRIMARY KEY (guild_id, node));
