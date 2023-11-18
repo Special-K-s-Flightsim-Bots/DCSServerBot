6dcb415276f7298cd3be186d780c72853e625b3d
karel26 <dcs.specialk@gmail.com>
2023-11-18 10:16:21 +0100

    BUGFIX: - Competitive: self_kills created an exception

--- plugins/competitive/listener.py
+++ plugins/competitive/listener.py
@@ -148,8 +148,7 @@ class CompetitiveListener(EventListener):
     def calculate_rating(r: Rating) -> float:
         return round(r.mu - 3.0 * r.sigma, 2)
 
-    @event(name="onGameEvent")
-    async def onGameEvent(self, server: Server, data: dict) -> None:
+    async def _onGameEvent(self, server: Server, data: dict) -> None:
         def print_crew(players: list[Player]) -> str:
             return ' and '.join([p.name for p in players])
 
@@ -160,7 +159,7 @@ class CompetitiveListener(EventListener):
             # self-kill
             if data['arg1'] == data['arg4']:
                 data['eventName'] = 'self_kill'
-                await self.onGameEvent(server, data)
+                await self._onGameEvent(server, data)
             # Multi-crew - pilot and all crew members gain points
             killers = server.get_crew_members(server.get_player(id=data['arg1']))
             victims = server.get_crew_members(server.get_player(id=data['arg4']))
@@ -207,6 +206,10 @@ class CompetitiveListener(EventListener):
                 for player in players:
                     match.player_dead(player)
                     del self.in_match[server.name][player.ucid]
+
+    @event(name="onGameEvent")
+    async def onGameEvent(self, server: Server, data: dict) -> None:
+        await self._onGameEvent(server, data)
 
     @chat_command(name="skill", help="Show your TrueSkill")
     async def skill(self, server: Server, player: Player, params: list[str]):
