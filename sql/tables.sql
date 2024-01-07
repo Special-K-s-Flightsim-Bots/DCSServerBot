a12871d47da84219332bdbf27f4ad225b0194bbd
karel26 <41538366+karel26@users.noreply.github.com>
2024-01-07 08:20:38 +0100

    missed correct version in tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS version (version TEXT PRIMARY KEY);
-INSERT INTO version (version) VALUES ('v3.0') ON CONFLICT (version) DO NOTHING;
+INSERT INTO version (version) VALUES ('v3.5') ON CONFLICT (version) DO NOTHING;
 CREATE TABLE IF NOT EXISTS plugins (plugin TEXT PRIMARY KEY, version TEXT NOT NULL);
 CREATE TABLE IF NOT EXISTS message_persistence (server_name TEXT NOT NULL, embed_name TEXT NOT NULL, embed BIGINT NOT NULL, PRIMARY KEY (server_name, embed_name));
 CREATE TABLE IF NOT EXISTS nodes (guild_id BIGINT NOT NULL, node TEXT NOT NULL, master BOOLEAN NOT NULL, last_seen TIMESTAMP DEFAULT NOW(), PRIMARY KEY (guild_id, node));
