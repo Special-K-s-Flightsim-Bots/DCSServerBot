b3b4c27300fa586275345d579e45bdbdf9c8dfc2
karel26 <dcs.specialk@gmail.com>
2024-01-24 21:57:42 +0100

    Version change to 3.0.2.0

--- services/dashboard/service.py
+++ services/dashboard/service.py
@@ -36,10 +36,11 @@ class HeaderWidget:
         grid.add_column(justify="center", ratio=1)
         grid.add_column(justify="right")
         message = f"[b]"
-        if self.node.master and len(self.node.all_nodes) > 0:
-            message += "Cluster Master | "
-        elif not self.node.master:
-            message += "Cluster Agent | "
+        if len(self.node.all_nodes) > 1:
+            if self.node.master:
+                message += "Cluster Master | "
+            else:
+                message += "Cluster Agent | "
         message += (f"DCSServerBot Version {self.node.bot_version}.{self.node.sub_version} | "
                     f"DCS Version {self.service.dcs_version}[/]")
         grid.add_row(message, datetime.now().ctime().replace(":", "[blink]:[/]"))
