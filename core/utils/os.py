dfd62d1fa16e468811189e8a38b9b65e00cb5649
karel26 <dcs.specialk@gmail.com>
2024-02-09 10:59:25 +0100

    CHANGES: - Optimized rmtree() to be used everywhere - Some smaller exception handling changes - Restructured requirements.txt for better readability

--- core/utils/os.py
+++ core/utils/os.py
@@ -1,16 +1,18 @@
 import os
+from pathlib import Path
+
 import aiohttp
 import ipaddress
 import psutil
 import socket
+import stat
 import sys
 if sys.platform == 'win32':
     import pywintypes
     import win32api
 
 from contextlib import closing, suppress
-from typing import Optional
-
+from typing import Optional, Union
 
 API_URLS = [
     'https://api4.my-ip.io/ip',
@@ -21,7 +23,8 @@ __all__ = [
     "is_open",
     "get_public_ip",
     "find_process",
-    "get_windows_version"
+    "get_windows_version",
+    "safe_rmtree"
 ]
 
 
@@ -66,3 +69,24 @@ def get_windows_version(cmd: str) -> Optional[str]:
     except pywintypes.error:
         version = None
     return version
+
+
+def safe_rmtree(path: Union[str, Path]):
+    # if path is a single file, delete that
+    if os.path.isfile(path):
+        os.chmod(path, stat.S_IWUSR)
+        os.remove(path)
+        return
+    # otherwise delete the tree
+    elif os.path.isdir(path):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                filename = os.path.join(root, name)
+                os.chmod(filename, stat.S_IWUSR)
+                os.remove(filename)
+            for name in dirs:
+                dirname = os.path.join(root, name)
+                os.chmod(dirname, stat.S_IWUSR)
+                os.rmdir(dirname)
+        os.chmod(path, stat.S_IWUSR)
+        os.rmdir(path)
