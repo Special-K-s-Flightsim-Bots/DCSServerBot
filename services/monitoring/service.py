02b132cc53fdad486a597fb2eea4a1d4b793f63c
karel26 <dcs.specialk@gmail.com>
2024-02-15 19:22:00 +0100

    BUGFIXES: - Server dead.. again

--- services/monitoring/service.py
+++ services/monitoring/service.py
@@ -139,7 +139,7 @@ class MonitoringService(Service):
             if server.is_remote or server.status in [Status.UNREGISTERED, Status.SHUTDOWN]:
                 continue
             # check if the process is dead (on load it might take some seconds for the process to appear)
-            if not await server.is_running():
+            if server.process and not await server.is_running():
                 message = f'Server "{server.name}" died. Setting state to SHUTDOWN.'
                 self.log.warning(message)
                 server.status = Status.SHUTDOWN
