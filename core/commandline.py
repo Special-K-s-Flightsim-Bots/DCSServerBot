ea1991ec5a8621a1dbf9eefe9e07845dffaf32fc
karel26 <dcs.specialk@gmail.com>
2024-05-01 11:04:30 +0200

    BUGFIX: Commandline args were overwritten in update.py

--- core/commandline.py
+++ core/commandline.py
@@ -14,13 +14,12 @@ if not COMMAND_LINE_ARGS:
     program = os.path.basename(sys.argv[0])
     parser = argparse.ArgumentParser(prog=program, description="Welcome to DCSServerBot!",
                                      epilog='If unsure about the parameters, please check the documentation.')
+    parser.add_argument('-n', '--node', help='Node name', default=platform.node())
+    parser.add_argument('-c', '--config', help='Path to configuration', default='config')
     if program == 'run.py':
-        parser.add_argument('-n', '--node', help='Node name', default=platform.node())
         parser.add_argument('-x', '--noupdate', action='store_true', help='Do not autoupdate')
-        parser.add_argument('-c', '--config', help='Path to configuration', default='config')
         parser.add_argument('-s', '--secret', action='store_true', help='Reveal all stored passwords')
     elif program == 'update.py':
-        parser.add_argument('-n', '--node', help='Node name', default=platform.node())
         parser.add_argument('-d', '--delete', action='store_true', help='remove obsolete local files')
         parser.add_argument('-r', '--no-restart', action='store_true', default=False,
                             help="don't start DCSServerBot after the update")
