671b5633bdd5e8dae3e649547446d7eb0df4efcb
karel26 <dcs.specialk@gmail.com>
2024-05-05 10:21:54 +0200

    CHANGES: - Rewrite of the SRS plugin - Timeout increase and small refactoring on server registering - Cloud logger is a root logger now

--- plugins/srs/lua/callbacks.lua
+++ plugins/srs/lua/callbacks.lua
@@ -9,8 +9,10 @@ function dcs_srs.onPlayerTryChangeSlot(playerID, side, slotID)
     local name = net.get_player_info(playerID, 'name')
     local srs = dcsbot.srs[name]
     if srs == nil then
+        log.write('DCSServerBot', log.DEBUG, 'No player found with name ' .. name .. ' in the SRS table.')
         return
     end
+    log.write('DCSServerBot', log.DEBUG, 'Player found in the SRS table, status is ' .. tostring(srs))
     if side == 1 or side == 2 and srs == false then
         net.send_chat_to("You need to use SRS to play on this server!", playerID)
         return false
