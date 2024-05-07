fff1be2aff0e5c8be361ebaed6980dd007c42839
karel26 <dcs.specialk@gmail.com>
2024-05-08 00:00:08 +0200

    Revert "Remove unwanted files"
    This reverts commit bc72d3d0cc44c020ae2007c5ffd3aebec39e7a22.

--- plugins/community/db/tables.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS all_servers(id SERIAL PRIMARY KEY, server_name TEXT NOT NULL, address TEXT NOT NULL, port INTEGER NOT NULL, num_players INTEGER NOT NULL, max_players INTEGER NOT NULL, geocontinent TEXT NOT NULL, geocountry TEXT NOT NULL, time TIMESTAMP DEFAULT (now() at time zone 'utc'));
-CREATE INDEX IF NOT EXISTS idx_all_servers_name ON all_servers(server_name);
-CREATE INDEX IF NOT EXISTS idx_all_servers_continent ON all_servers(geocontinent);
-CREATE INDEX IF NOT EXISTS idx_all_servers_country ON all_servers(geocountry);
