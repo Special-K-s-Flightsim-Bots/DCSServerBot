35b5eec5fab87003d3742b426a20afe8e41294fd
karel26 <dcs.specialk@gmail.com>
2024-06-21 16:08:46 +0200

    CHANGES: - Cleanup-service supports recursive deletes

--- services/cleanup/service.py
+++ services/cleanup/service.py
@@ -23,15 +23,17 @@ class CleanupService(Service):
     def do_cleanup(self, instance: Instance, now: time) -> None:
         for name, config in self.get_config(instance.server).items():
             self.log.debug(f"- Running cleanup for {name} ...")
-            directory = Path(os.path.expandvars(utils.format_string(config['directory'], node=self.node,
-                                                                    instance=instance)))
+            directory = Path(
+                os.path.expandvars(utils.format_string(config['directory'], node=self.node, instance=instance)))
             delete_after = int(config.get('delete_after', 30))
             threshold_time = now - delete_after * 86400
             patterns = config['pattern']
+            recursive = config.get('recursive', False)
             if not isinstance(patterns, list):
                 patterns = [patterns]
             for pattern in patterns:
-                for file_path in directory.glob(pattern):
+                search_pattern = f"**/{pattern}" if recursive else pattern
+                for file_path in directory.glob(search_pattern):
                     if os.path.getctime(file_path) < threshold_time:
                         self.log.debug(f"  => {file_path.name} is older then {delete_after} days, deleting ...")
                         utils.safe_rmtree(file_path)
