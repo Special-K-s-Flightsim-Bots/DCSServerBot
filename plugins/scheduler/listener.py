d298c1c488190c5e283d5987bce199f02d0cea55
karel26 <dcs.specialk@gmail.com>
2024-09-01 00:15:35 +0200

    CHANGES: - added idle_time to /timeleft
    BUGFIX:
    - Botless variant did not shut down plugins correctly
    - Commands: error on unloading

--- plugins/scheduler/listener.py
+++ plugins/scheduler/listener.py
@@ -30,6 +30,12 @@ class SchedulerListener(EventListener):
                     return delta, restart
                 else:
                     return 0, restart
+            elif 'idle_time' in restart and server.idle_since:
+                delta = int((datetime.now(timezone.utc) - server.idle_since).total_seconds())
+                if delta >= 0:
+                    return delta, restart
+                else:
+                    return 0, restart
             elif 'local_times' in restart:
                 min_time_difference = 86400
                 for t in restart['local_times']:
