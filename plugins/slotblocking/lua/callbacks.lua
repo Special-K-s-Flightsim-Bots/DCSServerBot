67c26f515b4da299669d727ccbe801553010a955
karel26 <dcs.specialk@gmail.com>
2024-09-10 00:23:29 +0200

    BUGFIX: - Slotblocking: server_message_full was not initialized with a default, if not set

--- plugins/slotblocking/lua/callbacks.lua
+++ plugins/slotblocking/lua/callbacks.lua
@@ -58,7 +58,7 @@ function slotblock.onPlayerTryConnect(addr, name, ucid, playerID)
         local current = #net.get_player_list() + 1
         if current >= (max - tonumber(cfg['slots'])) then
             if not is_vip(ucid) then
-                return false, cfg['message_server_full']
+                return false, cfg['message_server_full'] or 'The server is full, please try again later!'
             end
         end
     end
