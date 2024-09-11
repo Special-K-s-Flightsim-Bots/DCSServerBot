7b4269900bfa1ce5378023bd57217d8a19f4c8e1
karel26 <dcs.specialk@gmail.com>
2024-09-11 13:54:19 +0200

    Small doc & code changes

--- plugins/competitive/commands.py
+++ plugins/competitive/commands.py
@@ -17,52 +17,52 @@ _ = get_translation(__name__.split('.')[1])
 class Competitive(Plugin):
 
     async def install(self) -> bool:
-        if await super().install():
-            # we need to calculate the TrueSkill values for players
-            ratings: dict[str, Rating] = dict()
-            async with self.apool.connection() as conn:
-                async with conn.cursor(row_factory=dict_row) as cursor:
-                    size = 1000
-                    await cursor.execute("""
-                        SELECT p1.discord_id AS init_discord_id, m.init_id, 
-                               p2.discord_id AS target_discord_id, m.target_id 
-                        FROM missionstats m, players p1, players p2
-                        WHERE p1.ucid = m.init_id
-                        AND p2.ucid = m.target_id 
-                        AND event = 'S_EVENT_KILL' AND init_id != '-1' AND target_id != '-1'
-                        AND init_side <> target_side
-                        AND init_cat = 'Airplanes' AND target_cat = 'Airplanes'
-                        ORDER BY id
-                    """)
+        if not await super().install():
+            return False
+        # we need to calculate the TrueSkill values for players
+        ratings: dict[str, Rating] = dict()
+        async with self.apool.connection() as conn:
+            async with conn.cursor(row_factory=dict_row) as cursor:
+                size = 1000
+                await cursor.execute("""
+                    SELECT p1.discord_id AS init_discord_id, m.init_id, 
+                           p2.discord_id AS target_discord_id, m.target_id 
+                    FROM missionstats m, players p1, players p2
+                    WHERE p1.ucid = m.init_id
+                    AND p2.ucid = m.target_id 
+                    AND event = 'S_EVENT_KILL' AND init_id != '-1' AND target_id != '-1'
+                    AND init_side <> target_side
+                    AND init_cat = 'Airplanes' AND target_cat = 'Airplanes'
+                    ORDER BY id
+                """)
+                rows = await cursor.fetchmany(size=size)
+                while len(rows) > 0:
+                    for row in rows:
+                        init_id = row['init_discord_id'] if row['init_discord_id'] != -1 else row['init_id']
+                        target_id = row['target_discord_id'] if row['target_discord_id'] != -1 else row['target_id']
+                        if init_id not in ratings:
+                            ratings[init_id] = rating.create_rating()
+                        if target_id not in ratings:
+                            ratings[target_id] = rating.create_rating()
+                        ratings[init_id], ratings[target_id] = rating.rate_1vs1(
+                            ratings[init_id], ratings[target_id])
                     rows = await cursor.fetchmany(size=size)
-                    while len(rows) > 0:
+            async with conn.transaction():
+                for player_id, skill in ratings.items():
+                    if isinstance(player_id, str):
+                        await conn.execute("""
+                            INSERT INTO trueskill (player_ucid, skill_mu, skill_sigma) 
+                            VALUES (%s, %s, %s)
+                        """, (player_id, skill.mu, skill.sigma))
+                    else:
+                        cursor = await conn.execute("SELECT ucid FROM players WHERE discord_id = %s", (player_id, ))
+                        rows = await cursor.fetchall()
                         for row in rows:
-                            init_id = row['init_discord_id'] if row['init_discord_id'] != -1 else row['init_id']
-                            target_id = row['target_discord_id'] if row['target_discord_id'] != -1 else row['target_id']
-                            if init_id not in ratings:
-                                ratings[init_id] = rating.create_rating()
-                            if target_id not in ratings:
-                                ratings[target_id] = rating.create_rating()
-                            ratings[init_id], ratings[target_id] = rating.rate_1vs1(
-                                ratings[init_id], ratings[target_id])
-                        rows = await cursor.fetchmany(size=size)
-                async with conn.transaction():
-                    for player_id, skill in ratings.items():
-                        if isinstance(player_id, str):
                             await conn.execute("""
                                 INSERT INTO trueskill (player_ucid, skill_mu, skill_sigma) 
                                 VALUES (%s, %s, %s)
-                            """, (player_id, skill.mu, skill.sigma))
-                        else:
-                            cursor = await conn.execute("SELECT ucid FROM players WHERE discord_id = %s", (player_id, ))
-                            rows = await cursor.fetchall()
-                            for row in rows:
-                                await conn.execute("""
-                                    INSERT INTO trueskill (player_ucid, skill_mu, skill_sigma) 
-                                    VALUES (%s, %s, %s)
-                                """, (row[0], skill.mu, skill.sigma))
-            return True
-        return False
+                            """, (row[0], skill.mu, skill.sigma))
+        return True
 
     async def update_ucid(self, conn: psycopg.AsyncConnection, old_ucid: str, new_ucid: str) -> None:
         await conn.execute('UPDATE trueskill SET player_ucid = %s WHERE player_ucid = %s', (new_ucid, old_ucid))
