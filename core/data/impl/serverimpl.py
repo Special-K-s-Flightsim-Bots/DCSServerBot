c94a4fc23ad293fea9682ca063c1317221e09044
karel26 <dcs.specialk@gmail.com>
2024-09-15 15:15:31 +0200

    BUGFIX: - Missing import for file system scanning

--- core/data/impl/serverimpl.py
+++ core/data/impl/serverimpl.py
@@ -9,7 +9,6 @@ import shutil
 import socket
 import subprocess
 import sys
-import time
 import traceback
 
 if sys.platform == 'win32':
@@ -30,8 +29,8 @@ from dataclasses import dataclass, field
 from enum import Enum
 from pathlib import Path, PurePath
 from typing import Optional, TYPE_CHECKING, Union, Any
+from watchdog.events import FileSystemEventHandler, FileSystemEvent, FileSystemMovedEvent
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
 
 # ruamel YAML support
@@ -41,7 +40,6 @@ yaml = YAML()
 if TYPE_CHECKING:
     from core import Instance
     from services.bot import DCSServerBot
-    from watchdog.events import FileSystemEvent, FileSystemMovedEvent
 
 DEFAULT_EXTENSIONS = {
     "LogAnalyser": {}
