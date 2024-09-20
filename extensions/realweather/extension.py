762869d57797a91f34252597456018bf547e643c
karel26 <dcs.specialk@gmail.com>
2024-09-20 20:13:56 +0200

    Version up to 3.0.3.26
    CHANGES:
    - RealWeather 2.0 support (beta) added

--- extensions/realweather/extension.py
+++ extensions/realweather/extension.py
@@ -5,9 +5,17 @@ import re
 import shutil
 import subprocess
 import tempfile
+import sys
 
 from core import Extension, MizFile, utils, DEFAULT_TAG, Server
 from typing import Optional
+
+# TOML
+if sys.version_info >= (3, 11):
+    import tomllib as tomli
+else:
+    import tomli
+import tomli_w
 
 __all__ = [
     "RealWeather",
@@ -38,6 +46,14 @@ class RealWeather(Extension):
         else:
             return self.config
 
+    @property
+    def config_path(self) -> str:
+        rw_home = os.path.expandvars(self.config['installation'])
+        if self.version.split('.')[0] == '1':
+            return os.path.join(rw_home, 'config.json')
+        else:
+            return os.path.join(rw_home, 'config.toml')
+
     @staticmethod
     def get_icao_code(filename: str) -> Optional[str]:
         index = filename.find('ICAO_')
@@ -46,28 +62,25 @@ class RealWeather(Extension):
         else:
             return None
 
-    async def beforeMissionLoad(self, filename: str) -> tuple[str, bool]:
-        rw_home = os.path.expandvars(self.config['installation'])
-        tmpfd, tmpname = tempfile.mkstemp()
-        os.close(tmpfd)
+    async def generate_config_1_0(self, input_mission: str, output_mission: str, cwd: str):
         try:
-            with open(os.path.join(rw_home, 'config.json'), mode='r', encoding='utf-8') as infile:
+            with open(self.config_path, mode='r', encoding='utf-8') as infile:
                 cfg = json.load(infile)
         except json.JSONDecodeError as ex:
-            raise RealWeatherException(f"Error while reading {os.path.join(rw_home, 'config.json')}: {ex}")
-        config = self.get_config(filename)
+            raise RealWeatherException(f"Error while reading {self.config_path}: {ex}")
+        config = await asyncio.to_thread(self.get_config, input_mission)
         # create proper configuration
         for name, element in cfg.items():
             if name == 'files':
-                element['input-mission'] = filename
-                element['output-mission'] = tmpname
+                element['input-mission'] = input_mission
+                element['output-mission'] = output_mission
                 element['log'] = config.get('files', {}).get('log', 'logfile.log')
             elif name in config:
                 if isinstance(config[name], dict):
                     element |= config[name]
                 else:
                     cfg[name] = config[name]
-        icao = self.get_icao_code(filename)
+        icao = self.get_icao_code(input_mission)
         if icao and icao != self.config.get('metar', {}).get('icao'):
             cfg |= {
                 "metar": {
@@ -75,9 +88,52 @@ class RealWeather(Extension):
                 }
             }
             self.config['metar'] = {"icao": icao}
-        cwd = await self.server.get_missions_dir()
         with open(os.path.join(cwd, 'config.json'), mode='w', encoding='utf-8') as outfile:
             json.dump(cfg, outfile, indent=2)
+
+    async def generate_config_2_0(self, input_mission: str, output_mission: str, cwd: str):
+        tmpfd, tmpname = tempfile.mkstemp()
+        os.close(tmpfd)
+        try:
+            with open(self.config_path, mode='rb') as infile:
+                cfg = tomli.load(infile)
+        except tomli.TOMLDecodeError as ex:
+            raise RealWeatherException(f"Error while reading {self.config_path}: {ex}")
+        config = await asyncio.to_thread(self.get_config, input_mission)
+        # create proper configuration
+        for name, element in cfg.items():
+            if name == 'realweather':
+                element |= config.get('realweather', {"mission": {}})
+                element['mission'] |= config.get('mission', {}) | {
+                    "input": input_mission,
+                    "output": output_mission
+                }
+            elif name in config:
+                if isinstance(config[name], dict):
+                    element |= config[name]
+                else:
+                    cfg[name] = config[name]
+        icao = self.get_icao_code(input_mission)
+        if icao and icao != self.config.get('weather', {}).get('icao'):
+            cfg |= {
+                "weather": {
+                    "icao": icao
+                }
+            }
+            self.config['weather'] = {"icao": icao}
+        with open(os.path.join(cwd, 'config.toml'), mode='wb') as outfile:
+            tomli_w.dump(cfg, outfile)
+
+    async def beforeMissionLoad(self, filename: str) -> tuple[str, bool]:
+        tmpfd, tmpname = tempfile.mkstemp()
+        os.close(tmpfd)
+        cwd = await self.server.get_missions_dir()
+
+        if self.version.split('.')[0] == '1':
+            await self.generate_config_1_0(filename, tmpname, cwd)
+        else:
+            await self.generate_config_2_0(filename, tmpname, cwd)
+        rw_home = os.path.expandvars(self.config['installation'])
 
         def run_subprocess():
             process = subprocess.Popen([os.path.join(rw_home, 'realweather.exe')],
@@ -138,8 +194,8 @@ class RealWeather(Extension):
         else:
             self.log.error("DCS Realweather < 1.9.x not supported, please upgrade!")
             return False
-        if not os.path.exists(os.path.join(rw_home, 'config.json')):
-            self.log.error(f'No config.json found in {rw_home}')
+        if not os.path.exists(self.config_path):
+            self.log.error(f'No {os.path.basename(self.config_path)} found in {rw_home}')
             return False
         return True
 
