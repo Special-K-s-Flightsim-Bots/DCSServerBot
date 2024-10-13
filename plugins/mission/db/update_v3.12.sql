4da65664ff59c09aa21e9eddd8e2cb80b47380c7
karel26 <41538366+karel26@users.noreply.github.com>
2024-10-13 08:20:00 +0200

    Sql change

--- plugins/mission/db/update_v3.12.sql
+++ plugins/mission/db/update_v3.12.sql
@@ -1,3 +1,5 @@
+DROP TRIGGER IF EXISTS player_hist_change ON player;
 CREATE OR REPLACE FUNCTION player_hist_change() RETURNS trigger AS $$ BEGIN INSERT INTO players_hist(ucid, discord_id, name, manual) SELECT OLD.ucid, OLD.discord_id, OLD.name, COALESCE(OLD.manual, FALSE); RETURN NEW; END; $$ LANGUAGE 'plpgsql';
+CREATE TRIGGER tgr_player_update AFTER UPDATE OF discord_id, name, manual ON players FOR EACH ROW EXECUTE PROCEDURE player_hist_change();
 UPDATE players SET manual = FALSE WHERE manual IS NULL;
 ALTER TABLE players ALTER COLUMN manual SET NOT NULL;
