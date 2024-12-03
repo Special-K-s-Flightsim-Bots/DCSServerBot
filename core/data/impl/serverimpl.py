54027ccc37152dddf0f2cafdf7e1557ba3852c2c
karel26 <41538366+karel26@users.noreply.github.com>
2024-12-03 21:31:26 +0100

    HOTFIX
    Don't auto-add missions that were moved to the .dcssb directory

--- core/data/impl/serverimpl.py
+++ core/data/impl/serverimpl.py
@@ -201,6 +201,8 @@ class ServerImpl(Server):
         missions = self.settings['missionList']
         i: int = 0
         for file in directory.rglob('*.miz'):
+            if '.dcssb' in str(file):
+                continue
             secondary = os.path.join(os.path.dirname(file), '.dcssb', os.path.basename(file))
             if str(file) not in missions and secondary not in missions:
                 missions.append(str(file))
