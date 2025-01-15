a5b21a9dd8393f78f31484a6602600a078696533
karel26 <41538366+karel26@users.noreply.github.com>
2025-01-15 14:17:07 +0100

    Added bash scripts to run the bot on Linux

--- install.sh
+++ install.sh
@@ -7,13 +7,24 @@ echo " | |) | (__\\__ \\__ \\/ -_) '_\\ V / -_) '_| _ \\/ _ \\  _|"
 echo " |___/ \\___|___/___/\\___|_|  \\_/\\___|_| |___/\\___/\\__|"
 echo
 
-# Check if Python is in PATH
-if ! command -v python &> /dev/null; then
-    echo "python is not in your PATH."
+# Check if Python is callable
+if ! python --version &> /dev/null; then
+    echo "Python is not installed or not in your PATH, or it's an invalid alias."
     echo "Please choose 'Add python to the environment' in your Python installer."
     echo "Press any key to continue..."
     read -n 1
     exit 9009
+fi
+
+# Extract Python version and ensure it's > 3.9
+python_version=$(python -c "import sys; print(f'{sys.version_info[0]}.{sys.version_info[1]}')")
+required_version="3.9"
+
+# Compare Python versions
+if [ "$(printf '%s\n' "$required_version" "$python_version" | sort -V | head -n1)" != "$required_version" ]; then
+    echo "Python version must be > 3.9. Detected version: $python_version"
+    echo "Please upgrade your Python installation."
+    exit 1
 fi
 
 # Set virtual environment path
