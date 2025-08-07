6ea16f339867f47d751ad653a300e0657227cb96
karel26 <41538366+karel26@users.noreply.github.com>
2025-08-07 23:19:46 +0200

    Rolled-back setting remote servers to unregistered

--- core/data/server.py
+++ core/data/server.py
@@ -121,7 +121,7 @@ class Server(DataObject):
             new_status = Status(status)
         else:
             new_status = status
-        if new_status != self._status and new_status != Status.UNREGISTERED:
+        if new_status != self._status:
             self.log.debug(f"{self.name}: {self._status.name} => {new_status.name}")
             self.last_seen = datetime.now(timezone.utc)
             self._status = new_status
