3729c57f90c4ef79adab30a2056411c8be25660a
karel26 <41538366+karel26@users.noreply.github.com>
2025-08-10 23:00:58 +0200

    BUGFIX: - total sorties

--- plugins/restapi/commands.py
+++ plugins/restapi/commands.py
@@ -188,7 +188,7 @@ class RestAPI(Plugin):
                            ROUND(SUM(EXTRACT(EPOCH FROM(COALESCE(s.hop_off, NOW() AT TIME ZONE 'UTC') - s.hop_on))))::INTEGER AS "totalPlaytime",
                            ROUND(AVG(EXTRACT(EPOCH FROM(COALESCE(s.hop_off, NOW() AT TIME ZONE 'UTC') - s.hop_on))))::INTEGER AS "avgPlaytime",
                            SUM(CASE WHEN s.hop_off IS NULL THEN 1 ELSE 0 END) AS "activePlayers",
-                           SUM(s.takeoffs) AS "totalSorties",
+                           COUNT(*) AS "totalSorties",
                            SUM(s.kills) AS "totalKills",
                            SUM(s.deaths) AS "totalDeaths",
                            SUM(s.pvp) AS "totalPvPKills",
