c3f3117a3e98e2b20325cf21bb5fb7f79ddabfd6
karel26 <41538366+karel26@users.noreply.github.com>
2025-08-11 14:40:58 +0200

    CHANGES: - RestAPI: materialized view for serverstats added

--- plugins/restapi/db/tables.sql
+++ plugins/restapi/db/tables.sql
@@ -1,5 +1,5 @@
 CREATE MATERIALIZED VIEW mv_serverstats AS
-SELECT COUNT(DISTINCT s.player_ucid) AS "totalPlayers",
+SELECT COUNT(DISTINCT p.ucid) AS "totalPlayers",
       ROUND(SUM(EXTRACT(EPOCH FROM(COALESCE(s.hop_off, NOW() AT TIME ZONE 'UTC') - s.hop_on))) / 3600)::INTEGER AS "totalPlaytime",
       ROUND(AVG(EXTRACT(EPOCH FROM(COALESCE(s.hop_off, NOW() AT TIME ZONE 'UTC') - s.hop_on))))::INTEGER AS "avgPlaytime",
        SUM(CASE WHEN s.hop_off IS NULL THEN 1 ELSE 0 END) AS "activePlayers",
@@ -9,4 +9,5 @@ SELECT COUNT(DISTINCT s.player_ucid) AS "totalPlayers",
        SUM(s.pvp) AS "totalPvPKills",
        SUM(s.deaths_pvp) AS "totalPvPDeaths",
        NOW() AT TIME ZONE 'UTC' as "timestamp"
-FROM statistics s;
+FROM players p JOIN statistics s
+ON p.ucid = s.player_ucid;
