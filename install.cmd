69259723f1e52607239406b57017c2a799056ace
karel26 <41538366+karel26@users.noreply.github.com>
2025-09-14 20:55:51 +0200

    BUGFIX: removed ()

--- install.cmd
+++ install.cmd
@@ -15,7 +15,7 @@ if %ERRORLEVEL% EQU 9009 (
 )
 SET VENV=%USERPROFILE%\.dcssb
 if not exist "%VENV%" (
-    echo Creating the Python Virtual Environment (this might take some time ...)
+    echo Creating the Python Virtual Environment ...
     python -m venv "%VENV%"
     "%VENV%\Scripts\python.exe" -m pip install --upgrade pip
     "%VENV%\Scripts\pip" install pip-tools
