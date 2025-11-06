c73286d68f5c269807313d7dd7ff374a7bfdea90
karel26 <41538366+karel26@users.noreply.github.com>
2025-11-06 20:31:20 +0100

    CHANGES: - We can work with the latest PIP again.

--- install.cmd
+++ install.cmd
@@ -19,7 +19,7 @@ python -c "import sys; sys.exit(0 if sys.version_info >= (3,10) else 1)" >NUL 2>
 if errorlevel 1 (
     echo.
     echo ***  ERROR  ***
-    echo DCSServerBot requires Python >= 3.10 and < 3.14.
+    echo DCSServerBot requires Python >= 3.10.
     exit /B 1
 )
 
@@ -27,7 +27,7 @@ SET VENV=%USERPROFILE%\.dcssb
 if not exist "%VENV%" (
     echo Creating the Python Virtual Environment ...
     python -m venv "%VENV%"
-    "%VENV%\Scripts\python.exe" -m pip install --upgrade pip==25.2
+    "%VENV%\Scripts\python.exe" -m pip install --upgrade pip
     "%VENV%\Scripts\pip" install -r requirements.txt
 )
 "%VENV%\Scripts\python" install.py %*
