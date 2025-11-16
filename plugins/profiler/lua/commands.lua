12954c72b64851b0144254ddeb2944d48615278e
karel26 <41538366+karel26@users.noreply.github.com>
2025-11-16 11:17:28 +0100

    CHANGES: - Added verbose switch to /profiler start

--- plugins/profiler/lua/commands.lua
+++ plugins/profiler/lua/commands.lua
@@ -9,18 +9,10 @@ end
 
 function dcsbot.startProfiling(json)
     log.write('DCSServerBot', log.DEBUG, 'Profiler: startProfiling()')
-    if json.verbose then
-        net.dostring_in('mission', 'a_do_script("start_profiling(\\"' .. json.channel .. '\\", true)")')
-    else
-        net.dostring_in('mission', 'a_do_script("start_profiling(\\"-1\\", false)")')
-    end
+    net.dostring_in('mission', 'a_do_script("start_profiling(\\"' .. (json.channel or -1) .. '\\", ' .. tostring(json.verbose or false) .. ')")')
 end
 
 function dcsbot.stopProfiling(json)
     log.write('DCSServerBot', log.DEBUG, 'Profiler: stopProfiling()')
-    if json.channel then
-        net.dostring_in('mission', 'a_do_script("stop_profiling(\\"' .. json.channel .. '\\")")')
-    else
-        net.dostring_in('mission', 'a_do_script("stop_profiling()")')
-    end
+    net.dostring_in('mission', 'a_do_script("stop_profiling(\\"' .. (json.channel or -1) .. '\\")")')
 end
