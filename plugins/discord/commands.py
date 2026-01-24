091c15bb68052b195273e34975aaeab3999c5d3d
karel26 <41538366+karel26@users.noreply.github.com>
2026-01-24 16:20:34 +0100

    ENHANCEMENTS: - Discord: can send messages on member joins / leaves and role changes now.

--- plugins/discord/commands.py
+++ plugins/discord/commands.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 from core import Plugin, command, utils, get_translation
 from discord import app_commands
@@ -9,6 +10,13 @@ _ = get_translation(__name__.split('.')[1])
 
 
 class Discord(Plugin):
+
+    async def on_ready(self):
+        config = self.get_config()
+        for role_id in config.get('roles', {}).keys():
+            role = self.bot.get_role(role_id)
+            if not role:
+                self.log.warning(f"{self.__class__.__name__}: Role {role_id} not found.")
 
     @command(name='clear', description=_('Clear Discord messages'))
     @app_commands.guild_only()
@@ -74,6 +82,55 @@ class Discord(Plugin):
                 _("You don't have permission to remove role {role} from {member}.").format(
                     role=role.name, member=member.display_name), ephemeral=True)
 
+    async def _send_message(self, member: discord.Member, config: dict):
+        message = config['message']
+        if 'mention' in config:
+            mention_role = self.bot.get_role(config['mention'])
+            if not mention_role:
+                self.log.warning(
+                    f"{self.__class__.__name__}: Mention role {config['mention']} not found.")
+            else:
+                message = mention_role.mention + ' ' + message
+        channel_id = config.get('channel', -1)
+        channel = self.bot.get_channel(channel_id) if channel_id != -1 else member
+        await channel.send(message.format(name=member.display_name, mention=member.mention))
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        config = self.get_config()
+        if 'on_join' in config:
+            await self._send_message(member, config['on_join'])
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: discord.Member):
+        config = self.get_config()
+        for role_id in config.get('roles', {}).keys():
+            role = self.bot.get_role(role_id)
+            if role and role in member.roles:
+                config_role = config['roles'][role.id]
+                if 'on_leave' in config_role:
+                    await self._send_message(member, config_role['on_leave'])
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        before_roles = set(x.id for x in before.roles)
+        after_roles = set(x.id for x in after.roles)
+
+        config = self.get_config()
+        for role_id in config.get('roles', {}).keys():
+            role = self.bot.get_role(role_id)
+            if not role:
+                continue
+            # check removed roles
+            if role.id in (before_roles - after_roles):
+                config_role = config['roles'][role.id]
+                if 'on_remove' in config_role:
+                    await self._send_message(after, config_role['on_remove'])
+            # check added roles
+            if role.id in (after_roles - before_roles):
+                config_role = config['roles'][role.id]
+                if 'on_add' in config_role:
+                    await self._send_message(after, config_role['on_add'])
 
 
 async def setup(bot: DCSServerBot):
