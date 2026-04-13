21b8271258368bca9cfa0ec17dbe126883abaa9b
karel26 <41538366+karel26@users.noreply.github.com>
2026-04-13 13:33:18 +0200

    BUGFIX
    Music: duplicate songs could be played over each other

--- services/music/radios/srs.py
+++ services/music/radios/srs.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import os
 import psutil
 
@@ -16,32 +17,35 @@ class SRSRadio(Radio):
         self.process: psutil.Process | None = None
 
     async def play(self, file: str) -> None:
-        if self.current and self.process:
-            await self.skip()
-        if self.server.status != Status.RUNNING:
-            await self.stop()
-            return
-
-        self.log.debug(f"Playing {file} ...")
-
         extension = cast(SRS, self.server.extensions.get('SRS'))
         if not extension:
             self.log.error("SRS extension not found, can't play music.")
             return
 
+        if self.server.status != Status.RUNNING:
+            await self.stop()
+            return
+
+        # skip any song (if running)
+        await self.skip()
+
+        self.log.debug(f"Playing {file} ...")
+
         try:
-            self.process = await extension.play_external_audio(self.config, file=file)
+            proc = await extension.play_external_audio(self.config, file=file)
+            self.process = proc
+            self.current = get_tag(file).title or os.path.basename(file)
             coalition = Coalition.BLUE if int(self.config['coalition']) == 2 else Coalition.RED
             server_config = self.service.get_config(self.server)
             if 'popup' in server_config:
                 kwargs = self.config.copy()
-                kwargs['song'] = get_tag(file).title or os.path.basename(file)
+                kwargs['song'] = self.current
                 await self.server.sendPopupMessage(coalition, utils.format_string(server_config['popup'], **kwargs))
             if 'chat' in server_config:
                 kwargs = self.config.copy()
-                kwargs['song'] = get_tag(file).title or os.path.basename(file)
+                kwargs['song'] = self.current
                 await self.server.sendChatMessage(Coalition.ALL, utils.format_string(server_config['chat'], **kwargs))
-            await asyncio.to_thread(self.process.wait)
+            await asyncio.to_thread(proc.wait)
         except Exception as ex:
             self.log.exception(ex)
         finally:
@@ -50,9 +54,15 @@ class SRSRadio(Radio):
 
     async def skip(self) -> None:
         if self.process and self.process.is_running():
-            self.process.terminate()
-            self.current = None
-            self.process = None
+            try:
+                self.process.terminate()
+                await asyncio.to_thread(self.process.wait, 5)
+            except (psutil.TimeoutExpired, psutil.NoSuchProcess):
+                with contextlib.suppress(psutil.NoSuchProcess):
+                    self.process.kill()
+            finally:
+                self.current = None
+                self.process = None
 
     async def stop(self) -> None:
         if self.queue_worker.is_running():
