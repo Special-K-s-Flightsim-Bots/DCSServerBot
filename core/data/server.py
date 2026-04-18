03b5b89e3f88fd670db5c619785454f0c38e7d9e
karel26 <41538366+karel26@users.noreply.github.com>
2026-04-18 22:42:37 +0200

    CHANGES: - increased the default timeout to talk to DCS to 10s

--- core/data/server.py
+++ core/data/server.py
@@ -317,7 +317,7 @@ class Server(DataObject, ABC):
     async def startup(self, modify_mission: bool | None = True, use_orig: bool | None = True) -> None:
         raise NotImplementedError()
 
-    async def send_to_dcs_sync(self, message: dict, timeout: int | None = 5) -> dict | None:
+    async def send_to_dcs_sync(self, message: dict, timeout: int | None = 10) -> dict | None:
         with PerformanceLog(f"DCS: dcsbot.{message['command']}()"):
             future = self.bus.loop.create_future()
             token = 'sync-' + str(uuid.uuid4())
