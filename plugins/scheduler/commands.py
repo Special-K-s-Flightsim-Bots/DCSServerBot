a488709474e8a30d826cda6ffa6623a2f11a3e0b
karel26 <41538366+karel26@users.noreply.github.com>
2026-05-15 18:17:02 +0200

    Implement maintenance flag for server shutdown
    Added maintenance flag handling during server shutdown.

--- plugins/scheduler/commands.py
+++ plugins/scheduler/commands.py
@@ -457,6 +457,10 @@ class Scheduler(Plugin[SchedulerListener]):
 
         if method == 'shutdown' or rconf.get('shutdown', False):
             self.log.debug(f"{self.__cog_name__}: Shutting down server {server.name} ...")
+            # set the maintenance flag on shutdown unless specified otherwise
+            if method == 'shutdown':
+                server.maintenance = rconf.get('maintenance', True)
+            # shut down the server
             await self.teardown_dcs(server)
             if method == 'shutdown':
                 return
